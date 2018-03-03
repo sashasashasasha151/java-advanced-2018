0fc3a2037730795b20f224a6703b6f53122e13ea
Sasha Pakulev <sashasashasasha151@gmail.com>
2018-03-03 14:25:01 +0300

    homework 3

---
+++ java/ru/ifmo/rain/pakulev/student/StudentDB.java
@@ -0,0 +1,58 @@
+package ru.ifmo.rain.pakulev.student;
+
+import info.kgeorgiy.java.advanced.student.Student;
+import info.kgeorgiy.java.advanced.student.StudentQuery;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class StudentDB implements StudentQuery {
+
+    public List<String> getFirstNames(List<Student> students) {
+        return students.stream().map(Student::getFirstName).collect(Collectors.toList());
+    }
+
+    public List<String> getLastNames(List<Student> students) {
+        return students.stream().map(Student::getLastName).collect(Collectors.toList());
+    }
+
+    public List<String> getGroups(List<Student> students) {
+        return students.stream().map(Student::getGroup).collect(Collectors.toList());
+    }
+
+    public List<String> getFullNames(List<Student> students) {
+        return students.stream().map(student -> student.getFirstName() + " " + student.getLastName()).collect(Collectors.toList());
+    }
+
+    public Set<String> getDistinctFirstNames(List<Student> students) {
+        return students.stream().map(Student::getFirstName).collect(Collectors.toSet());
+    }
+
+    public String getMinStudentFirstName(List<Student> students) {
+        return students.stream().reduce((name, student) -> (name.getId() < student.getId()) ? name : student).get().getFirstName();
+    }
+
+    public List<Student> sortStudentsById(Collection<Student> students) {
+        return students.stream().sorted(Comparator.comparing(Student::getId)).collect(Collectors.toList());
+    }
+
+    public List<Student> sortStudentsByName(Collection<Student> students) {
+        return students.stream().sorted(Comparator.comparing(Student::getLastName).thenComparing(Student::getFirstName).thenComparing(Student::getId)).collect(Collectors.toList());
+    }
+
+    public List<Student> findStudentsByFirstName(Collection<Student> students, String name) {
+        return students.stream().filter(student -> student.getFirstName().equals(name)).collect(Collectors.toList());
+    }
+
+    public List<Student> findStudentsByLastName(Collection<Student> students, String name) {
+        return students.stream().filter(student -> student.getLastName().equals(name)).collect(Collectors.toList());
+    }
+
+    public List<Student> findStudentsByGroup(Collection<Student> students, String group) {
+        return students.stream().filter(student -> student.getGroup().equals(group)).sorted(Comparator.comparing(Student::getLastName).thenComparing(Student::getFirstName).thenComparing(Student::getId)).collect(Collectors.toList());
+    }
+
+    public Map<String, String> findStudentNamesByGroup(Collection<Student> students, String group) {
+        return students.stream().filter(student -> student.getGroup().equals(group)).sorted(Comparator.comparing(Student::getLastName).thenComparing(Student::getFirstName).thenComparing(Student::getId)).collect(Collectors.toMap(Student::getLastName, Student::getFirstName, (a,b)->a));
+    }
+}
--- java/ru/ifmo/rain/pakulev/walk/Test.java
+++
@@ -1,7 +0,0 @@
-package ru.ifmo.rain.pakulev.walk;
-
-public class Test {
-    public static void main(String[] args) {
-        new Walk().main(null);
-    }
-}
