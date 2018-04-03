87f68822afea0607c977e543f5d5548e997405a6
Sasha Pakulev <sashasashasasha151@gmail.com>
2018-04-03 22:09:27 +0300

    homework 7 complete

--- java/ru/ifmo/rain/pakulev/concurrent/Concurrent.java
+++ java/ru/ifmo/rain/pakulev/concurrent/Concurrent.java
@@ -1,82 +1,57 @@
-//package ru.ifmo.rain.pakulev.concurrent;
-//
-//import info.kgeorgiy.java.advanced.concurrent.ScalarIP;
-//
-//import java.util.ArrayList;
-//import java.util.Comparator;
-//import java.util.List;
-//import java.util.function.Predicate;
-//
-//
-//public class Concurrent implements ScalarIP {
-//
-//    private class Make<T> implements Runnable {
-//        List<? extends T> list;
-//        Comparator<? super T> comparator;
-//
-//        Make(List<? extends T> list, Comparator<? super T> comparator) {
-//            this.list = list;
-//            this.comparator = comparator;
-//        }
-//
-//
-//        @Override
-//        public void run() {
-//            System.out.println(list.stream().min(comparator).get());
-//        }
-//    }
-//
-//    public <T> T maximum(int i, List<? extends T> list, Comparator<? super T> comparator) throws InterruptedException {
-//        if (list.size() <= i) {
-//            return list.stream().max(comparator).get();
-//        }
-//        //System.out.println("start");
-//        ArrayList<T> answer = new ArrayList<T>();
-//        ArrayList<Thread> threads = new ArrayList<Thread>();
-//        Object o = new Object();
-//
-//        final int blockSize = list.size() / i + 1;
-//        for (int j = 0; j < i; ++j) {
-//            int n = j;
-//            int sz = list.size();
-//            Thread t = new Thread(() -> {
-//                int l, r;
-//                synchronized (o) {
-//                    l = n * blockSize;
-//                    r = l + blockSize;
-//                }
-//                T max = list.get(l);
-//                for (int k = l + 1; k < r; ++k) {
-//                    if (k == sz) {
-//                        break;
-//                    }
-//                    max = comparator.compare(max, list.get(k)) > 0 ? max : list.get(k);
-//                }
-//                synchronized (o) {
-//                    answer.add(max);
-//                }
-//            });
-//            threads.add(t);
-//            t.start();
-//        }
-//
-//        for (Thread thread : threads) {
-//            thread.join();
-//        }
-//        //System.out.println("finish");
-//
-//        return answer.stream().max(comparator).get();
-//    }
-//
-//    public <T> T minimum(int i, List<? extends T> list, Comparator<? super T> comparator) throws InterruptedException {
-//        return list.get(0);
-//    }
-//
-//    public <T> boolean all(int i, List<? extends T> list, Predicate<? super T> predicate) throws InterruptedException {
-//        return false;
-//    }
-//
-//    public <T> boolean any(int i, List<? extends T> list, Predicate<? super T> predicate) throws InterruptedException {
-//        return false;
-//    }
-//}
+package ru.ifmo.rain.pakulev.concurrent;
+
+import info.kgeorgiy.java.advanced.concurrent.ScalarIP;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+
+public class Concurrent implements ScalarIP {
+
+    private <T, R> R mainFunction(int i, List<? extends T> list,
+                                  final Function<Stream<? extends T>, ? extends R> resultF,
+                                  final Function<Stream<? extends R>, ? extends R> resultR) throws InterruptedException {
+        if (list.size() <= i) {
+            return resultF.apply(list.stream());
+        }
+
+        ArrayList<R> answer = new ArrayList<R>(Collections.nCopies(i, null));
+        ArrayList<Thread> threads = new ArrayList<Thread>(Collections.nCopies(i, null));
+        int remainder = list.size() % i;
+        for (int j = 0; j < i; ++j) {
+            int blockSize = list.size() / i + (remainder-- > 0 ? 1 : 0);
+            final int k = j;
+            final int l = j * blockSize;
+            final int r = l + blockSize;
+            Thread t = new Thread(() -> {
+                answer.set(k, resultF.apply(list.subList(l, r < list.size() ? r : list.size()).stream()));
+            });
+            threads.set(k, t);
+            t.start();
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        return resultR.apply(answer.stream());
+    }
+
+    public <T> T maximum(int i, List<? extends T> list, Comparator<? super T> comparator) throws InterruptedException {
+        return mainFunction(i, list, stream -> stream.max(comparator).get(), stream -> stream.max(comparator).get());
+    }
+
+    public <T> T minimum(int i, List<? extends T> list, Comparator<? super T> comparator) throws InterruptedException {
+        return mainFunction(i, list, stream -> stream.min(comparator).get(), stream -> stream.min(comparator).get());
+    }
+
+    public <T> boolean all(int i, List<? extends T> list, Predicate<? super T> predicate) throws InterruptedException {
+        return mainFunction(i, list, stream -> stream.allMatch(predicate), stream -> stream.allMatch(p -> p));
+    }
+
+    public <T> boolean any(int i, List<? extends T> list, Predicate<? super T> predicate) throws InterruptedException {
+        return mainFunction(i, list, stream -> stream.anyMatch(predicate), stream -> stream.anyMatch(p -> p));
+    }
+}
