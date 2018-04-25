e6c57569ad176b370cffa1f069efbc1cd2369b30
Sasha Pakulev <sashasashasasha151@gmail.com>
2018-04-25 22:32:26 +0300

    something

--- java/ru/ifmo/rain/pakulev/mapper/ParallelMapperImpl.java
+++ java/ru/ifmo/rain/pakulev/mapper/ParallelMapperImpl.java
@@ -57,6 +57,7 @@ public class ParallelMapperImpl implements ParallelMapper {
             list = new ArrayList<>(Collections.nCopies(n, null));
             size = 0;
         }
+
         void set(int i, T value) {
             synchronized (this) {
                 list.set(i, value);
